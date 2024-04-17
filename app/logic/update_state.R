765cb8ddbac67a3a9223f2ecc81c7161b9681e90
Evan Cutler Anway <evan.anway@gmail.com>
2024-04-17 19:41:07 -0400

    clarify updating state to avoid namespace conflicts

--- app/logic/update_state.R
+++ app/logic/update_state.R
@@ -1,25 +1,25 @@
 update_state <- function(
-    df_states, label, species, var_x, var_y) {
+    df_states, new_label, new_species, new_var_x, new_var_y) {
   box::use(
     dplyr[bind_rows, mutate, case_match]
   )
 
-  if (label %in% df_states$label) {
+  if (new_label %in% df_states$label) {
     df_updated_states <- df_states |>
       mutate(
         species = case_match(
           label,
-          current_state ~ species,
+          new_label ~ new_species,
           .default = species
         ),
         var_x = case_match(
           label,
-          current_state ~ var_x,
+          new_label ~ new_var_x,
           .default = var_x
         ),
         var_y = case_match(
           label,
-          current_state ~ var_y,
+          new_label ~ new_var_y,
           .default = var_y
         )
       )
@@ -28,7 +28,8 @@ update_state <- function(
   }
 
   df_new_state <- data.frame(
-    label = label, species = species, var_x = var_x, var_y = var_y
+    label = new_label, species = new_species,
+    var_x = new_var_x, var_y = new_var_y
   )
 
   df_states |>
