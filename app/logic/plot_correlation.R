a3c3a45814e8da1d3ff61c2653ad4530f62e2abe
Evan Cutler Anway <evan.anway@gmail.com>
2024-04-17 16:38:44 -0400

    add line

--- app/logic/plot_correlation.R
+++ app/logic/plot_correlation.R
@@ -1,6 +1,6 @@
 plot_correlation <- function(df_data, x_lab, y_lab, correlation_coef) {
   box::use(
-    ggplot2[aes, ggplot, geom_point, labs],
+    ggplot2[aes, ggplot, geom_point, geom_smooth, labs],
     glue[glue],
   )
 
@@ -8,5 +8,6 @@ plot_correlation <- function(df_data, x_lab, y_lab, correlation_coef) {
 
   ggplot(df_data, aes(x = x, y = y)) +
     geom_point() +
+    geom_smooth(method = "lm",formula = y ~ x) +
     labs(title = "Correlation", subtitle = str_subtitle, x = x_lab, y = y_lab)
 }
--- app/view/plot.R
+++ app/view/plot.R
@@ -10,7 +10,7 @@ ui <- function(id) {
 }
 
 box::use(
-  shiny[moduleServer, renderPlot, reactive]
+  shiny[moduleServer, renderPlot, reactive, validate, need]
 )
 
 box::use(
@@ -22,6 +22,11 @@ server <- function(
     id, rct_df_selected, rct_var_x, rct_var_y, rct_correlation_coef) {
   moduleServer(id, function(input, output, session) {
     rct_gg_plot <- reactive({
+      validate(
+        need(rct_var_x(), "Loading, please wait"),
+        need(rct_var_y(), "Loading, please wait")
+      )
+
       rct_df_selected() |>
         plot_correlation(rct_var_x(), rct_var_y(), rct_correlation_coef())
     })
